5a90753568ca2d1abb9f145bf7cbec990a52aefb
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 11:05:08 +0500

    18

--- main.java
+++ main.java
@@ -1,72 +1,39 @@
+import java.util.Scanner;
+import java.lang.Integer;
+
+class UserInput{
+  private String binaryString_;
+  public String getUserInput(){
+    Scanner scanner = new Scanner(System.in);
+    binaryString_ = scanner.nextLine();
+    return binaryString_;
+}//getUserInput()
+}//UserInput class
+
 class Test{
-  public void printLineOne(){
-    for(int i = 1; i <= 90; i++){
-      if(i <= 12){
-	if(i % 2 != 0){System.out.print("*");
-	
-}//if(i%2 != 0)
-	else if(i%2 == 0){System.out.print(" ");
+  private String binaryString1_;
+  private String binaryString2_;
+  private int decimalSum;
+  private String binarySum;
+  public String getUserInput(){
+    UserInput input = new UserInput();
+    return input.getUserInput();
+      
+}//func
 
-}//elseif(i%2 ==0)
-}//if(i<=12)
-      else if(i>12){System.out.print("=");
-
-}//elseif(i>12)
-}//for() inside printLineOne()
-
-System.out.println("");
-
-}//printLineOne()
-
-  public void printLineTwo(){
-    for(int i = 1; i <= 90; i++){
-      if(i <= 12){
-	if(i % 2 == 0 && i != 12){System.out.print("*");
-	
-}//if(i%2==0) inside printTwo()
-	else if(i == 12){System.out.print(" ");
-
-}//elseif(i==12) inside printTwo()
-	else if(i % 2 != 0){
-	  System.out.print(" ");
-}//elseif (i%2 != 0) inside printTwo()
-
-}//if(i<=12) in printTwo()
-      else if(i > 12){ 
-	System.out.print("=");
-}//elseif(i > 12) inside printTwo()
-}//for() inside printLineTwo()
-System.out.println("");
-}//printLineTwo()
-
-  public void printEquals(){
-    for(int i = 1; i <= 90; i++){
-      System.out.print("=");
-}//for() inside printEquals()
-System.out.println("");
-}//printEquals()
-}//class Test{}
-
-class Helper{
-  public void helpPrint(){
-    Test test = new Test();
-    for(int i = 1; i <= 8; i++){
-      if(i <= 5){
-	test.printLineOne();
-	test.printLineTwo();
-}//if(i<=6) inside for() of helperPrint()
-
-      else if(i>5){
-	test.printEquals();
-	test.printEquals();
-}//elseif(i>5) inside for() of helpPrint()
-}//for() inside helpPrint()
-}//helpPrint()
-}//class Helper
+  public void binaryAddition(){
+    binaryString1_ = getUserInput();
+    binaryString2_ = getUserInput();
+    decimalSum = Integer.parseInt(binaryString1_,2) + Integer.parseInt(binaryString2_,2);
+      binarySum = Integer.toBinaryString(decimalSum);
+      System.out.println(binarySum);
+      System.out.println(decimalSum);
+}//binaryAddition
+}//Test class
 
 public class main{
   public static void main(String args[]){
-    Helper helper = new Helper();
-    helper.helpPrint();
-}//public Static Void Main()
-}//class main
+    Test test = new Test();
+    test.binaryAddition();
+}
+}
