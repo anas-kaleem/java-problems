d2649d837f3a9868801175fbbba329b111df2f68
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 11:07:17 +0500

    20

--- main.java
+++ main.java
@@ -1,20 +1,36 @@
+
 class Test{
-  private int num = 1;
+  
   private int number = 5;
+  private String binaryString = "";
   private int arr[] = {128,64};
   public void print(){
 
-    while(num == 1){
-      if(arr[1] == 0){
-        num = 0;
+    while(number != 0){
+      if(number == arr[0]){
+        binaryString = binaryString + '1';
+	number = arr[0] - number;
+	arr[0] = arr[1];
+        arr[1] = (arr[1]/2);
 }//1stIf
-      else if(num != 0){
-	System.out.print(arr[0]);
-        System.out.println(arr[1]);
+      else if((number < arr[0]) && (number >= arr[1])){
+	binaryString = binaryString + '1';
+	number = number - arr[1];
         arr[0] = arr[1];
         arr[1] = (arr[1]/2);
 }//1stElseifOF1stIf
+      else{
+	binaryString = binaryString + '0';
+        arr[0] = arr[1];
+        arr[1] = (arr[1]/2);
+	
+}//elseOF1stIf
+	System.out.print(arr[0]);
+        System.out.println(arr[1]);
 }//while(num ==1 )
+
+System.out.println("");
+System.out.printf("The binary form of the number %d is: %s ",number,binaryString);
 }//print()
 }//class Test
 
