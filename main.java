758154ef6dc593d0463690fb93901d7af1bc9089
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 11:00:22 +0500

    12

--- main.java
+++ main.java
@@ -1,36 +1,16 @@
-import java.util.Scanner;
-
-class UserInput{
-	private int number_;
-	public int takeUserInput(){
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		number_ = scanner.nextInt();
-		return number_;
+class ReverseString{
+	private String name = "Hello my name is anas";
+	public void reverseName(){
+		for(int i = (name.length()-1); i >=0 ; i--){
+			System.out.println(name.charAt(i));
+}
 }
 }
 
-class Partition{
-	private int inputNumber_;
-	
-	public void takeInput(){
-		UserInput input = new UserInput();
-		inputNumber_ = input.takeUserInput();
-		
-}
-	public void partitionNumber(){
-		takeInput();
-		
-		while(inputNumber_ != 0){
-			System.out.println(inputNumber_ % 10);
-			inputNumber_a = inputNumber_ / 10;
-}
-}
-}
 
 public class main{
 	public static void main(String args[]){
-		Partition partition = new Partition();
-		partition.partitionNumber();
+		ReverseString reverse = new ReverseString();
+		reverse.reverseName();
 }
 }
