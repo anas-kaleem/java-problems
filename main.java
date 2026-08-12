90172f132d593900771e6066852c496619801ac6
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 11:11:23 +0500

    25

--- main.java
+++ main.java
@@ -1,39 +1,36 @@
 import java.util.Scanner;
 
 class Test{
-  private int inputNum;
-  private int helpingNum;
-  private int decimalNum = 0;
+  private String inputNum;
+  private char helpingNum;
+  private int index = 0;
   private int power = 1;
+  
   private char arr[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-  private String hexaDecimal = "";
   public void func(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter the octal number: ");
-    inputNum = scanner.nextInt();
-    //octal to decimal converting.
-    while(inputNum > 0){
-      helpingNum = inputNum % 10;
-      decimalNum = (helpingNum * power) + decimalNum;
-      inputNum = inputNum/10;
-      power = power * 8;
-}//1st_while_loop_inside_func
-    System.out.println("The decimal number of octal after first while loop is: ");
-    System.out.println(decimalNum);
-    //decimal to hexadecimal converting.
-    while(decimalNum > 0){
-      helpingNum = decimalNum % 16;
-      hexaDecimal = (arr[helpingNum]) + hexaDecimal;
-      decimalNum = decimalNum / 16;
-}//2nd_while_loop_inside_func
-    System.out.println("hexadecimal number from second while loop is: ");
-    System.out.println(hexaDecimal);
+    System.out.print("Enter the hexadecimal number: ");
+    inputNum = scanner.nextLine();
+    for(int i = (inputNum.length() - 1); i >= 0; i--){
+      helpingNum = inputNum.charAt(i);
+      helpingNum = Character.toUpperCase(helpingNum);
+      for(int j = 0; j < arr.length; j++){
+        if(arr[j] == helpingNum){
+	  index = (j * power) + index;
+	  power = power * 16;
+}//if_inside_2nd_for
+}//2nd_for_inside_func()
+}//1st_for_inside_func()
+
+System.out.println("The decimal number is: ");
+System.out.print(index);
 }//func
+  
 }//class_Test
 
 public class main{
   public static void main(String args[]){
     Test test = new Test();
     test.func();
-}//void main()
-}//class main
+}
+}
