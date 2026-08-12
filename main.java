db4ba1c4ad6b014878cb64878a6ed0e6319349de
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 10:48:42 +0500

    3

--- main.java
+++ main.java
@@ -1,15 +1,15 @@
-class sumOfNumbers{
-	private int number1_ = 74;
-	private int number2_ = 36;
-	public int computeSum(){
-		return number1_ + number2_;
+class Division{
+	private int number1_ = 50;
+	private int number2_ = 3;
+	public void computeDivision(){
+		System.out.println("Expected output: ");
+		System.out.println(number1_/number2_);
 }
-	
 }
 
 public class main{
 	public static void main(String args[]){
-		sumOfNumbers sumObject = new sumOfNumbers();
-		System.out.println(sumObject.computeSum()); 
+		Division division = new Division();
+		division.computeDivision();
 }
 }
