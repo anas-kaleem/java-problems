6daab71b78eef879aa640e63f663f500349a5087
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 10:49:45 +0500

    4

--- main.java
+++ main.java
@@ -1,15 +1,30 @@
-class Division{
-	private int number1_ = 50;
-	private int number2_ = 3;
-	public void computeDivision(){
-		System.out.println("Expected output: ");
-		System.out.println(number1_/number2_);
+import java.util.Scanner;
+
+class userInput{
+	private int number_;
+	public int getUserInput(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		number_ = scanner.nextInt();
+		return number_;
+}
+}
+class Product{
+	private int number1_;
+	private int number2_;
+	private int product;
+	public void computeProduct(){
+		userInput input = new userInput();
+		number1_ = input.getUserInput();
+		number2_ = input.getUserInput();
+		product = number1_ * number2_;
+		System.out.println(product);
 }
 }
 
 public class main{
 	public static void main(String args[]){
-		Division division = new Division();
-		division.computeDivision();
+		Product product = new Product();
+		product.computeProduct();
 }
 }
