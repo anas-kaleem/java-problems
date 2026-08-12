821f230ae00516d878b065d73ae8e101e06a974a
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 11:02:20 +0500

    14

--- main.java
+++ main.java
@@ -1,20 +1,41 @@
-class Combination{
-	private int[] nNumbers_ =  {1,2,3,4};
-	public void printCombination(){
-		for(int i = 0; i <= 4; i++){
-		  for(int j =0; j<=4 ; i++){
-		    for(int k =0; k<=4;k++){
-		      if(i != j && j != k&& i != k){System.out.println(i + "" + j + "" +k);}
+//class starts here.
+class Processes{
+
+//private variables start.
+	private int[] processBurst = {16,8,5,10};
+	private int[] timer = {0,0,0,0};
+	private int timeSlice = 5;
+	private int endLoop = 0;
+	private int i = 0;
+//private variables end.
+
+//public methods start.
+	public void completionTime(){
+
+//loop through both arrays of class. update each value in both arrays. increment each value //of timer array by 5. and decrease each value of processBurst array by 5. last value of //processBurst is checked if decrementing it by 5 is less than or greater than 0. if it is //greater than zero then normal actitvities happen(adding and subtracting 5 in that index //of both arrays respectively). But if last value of processBurst is less than zero then //the value subtracted
+		while(endLoop != 4){
+			if((processBurst[i] - timeSlice) > 0){timer[i] = timer[i] + timeSlice; processBurst[i] = processBurst[i] - timeSlice;}
+
+			else{if(processBurst[i] != 0 &&(processBurst[i] - timeSlice) == 0){timer[i] = timer[i]+timeSlice; processBurst[i] = processBurst[i]+timeSlice; 			endLoop = endLoop + 1;}
+
+			else if(processBurst[i] > 0 && (processBurst[i] - timeSlice) == 0){timer[i] = timer[i] + processBurst[i]; processBurst[i] 					= 0; endLoop = endLoop+1;}
+			
+			else{break;}
 }
-}
-}
-}
-}
+		i++;	
+}//for loop ends here.
+
+
+for(int i =0; i <4 ; i++){System.out.println(processBurst[i]); System.out.print(timer[i]);}
+}//method ends here.
+
+
+}//class ends here.
+
 
 public class main{
-	public static void main(String args[]){
-		Combination combination = new Combination();
-
-		combination.printCombination();
+	public static void main(String [] args){
+		Processes processes = new Processes();
+		processes.completionTime();
 }
 }
