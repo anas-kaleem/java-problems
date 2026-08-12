61882e9f66c65fd7ddd7e99ed295e6224d6e7b8c
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-12 15:52:43 +0500

    64

--- main.java
+++ main.java
@@ -1,34 +1,58 @@
 import java.util.Scanner;
 
-class Comparison{
+class CommonDigits{
   private int number1;
   private int number2;
-  public void printResult(){
-    System.out.print(compareNumbers());
+  private int arr1[];
+  private int arr2[];
+  private boolean result;
+  public void print(){
+    System.out.print(isCommon());
 }
-  public int compareNumbers(){
+  public boolean isCommon(){
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter first number: ");
     number1 = scanner.nextInt();
     System.out.print("Enter second number: ");
     number2 = scanner.nextInt();
-    if(number1 == number2){return 0;}
+    
+    if((number1 >= 25 && number1 <= 75) && (number2 >= 25 && number2 <= 75)){
+      int i = 0;
+      int temp = number1;
+      while(temp > 0){
+        i++;
+        temp = temp/10;
+}//1st-while-in-if-of-isCommon()
 
-    else if((number1 % 6) == (number2 % 6)){
-      if(number1 < number2){return number1;}
-      else{return number2;}
+    arr1 = new int[i];
+    arr2 = new int[i];
+      for(int j = 0; j < i; j++){
+        arr1[j] = number1 % 10;
+        arr2[j] = number2 % 10;
+        number1 = number1 / 10;
+        number2 = number2 / 10;
+}//1st-for-loop-in-1st-if-in-isCommon()
+       
+      for(int k = 0; k < arr1.length; k++){
+        for(int m = 0; m < arr2.length; m++){
+          if(arr1[k] == arr2[m]){result = true;}
+         
 }
+}//2nd-for-loop-in-if-of-isCommon()
+}//1st-if-in-isCommon()
 
-    else{
-      if(number1 < number2){return number2;}
-      else{return number1;}
-}
-}
-}
+   else{System.out.print("number is not between range of 25 and 75");}
+
+   return result;
+}//boolean-isCommon()
+}//class-CommonDigits{}
+
 
 public class main{
+
   public static void main(String args[]){
-    Comparison comparison = new Comparison();
-    comparison.printResult();
+    CommonDigits common = new CommonDigits();
+    common.print();
 }
+
 }
