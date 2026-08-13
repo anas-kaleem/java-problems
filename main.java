e3edcef3af2484fa91fc6c3844a734fddec7f697
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-13 19:06:08 +0500

    71

--- main.java
+++ main.java
@@ -1,31 +1,33 @@
 import java.util.Scanner;
 
-class Length{
+class Text{
   private String text1;
   private String text2;
-  private int str1Length;
-  private int str2Length;
-  public void printSequence(){
+  private String result1 = "";
+  private String result2 = "";
+
+  public void concatenate(){
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter first text: ");
     text1 = scanner.nextLine();
     System.out.print("Enter second text: ");
     text2 = scanner.nextLine();
     
-    str1Length = text1.length();
-    str2Length = text2.length();
-    if(str1Length < str2Length){
-      System.out.print(text1 + " " +  text2 + " " + text1);
+    for(int i = 1; i < text1.length(); i++){
+      result1 = result1 + text1.charAt(i);
+}//1st-for
+
+    for(int i = 1; i < text2.length(); i++){
+      result2 = result2 + text2.charAt(i);
 }
-    else{
-      System.out.print(text2 + " " + text1 + " "+ text2);
-}
-}
-}
+
+    System.out.print(result1 + result2);
+}//void concatenate()
+}//class String
 
 public class main{
   public static void main(String args[]){
-    Length length = new Length();
-    length.printSequence();   
+    Text text = new Text();
+    text.concatenate();
 }
 }
