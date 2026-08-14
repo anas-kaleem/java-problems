f2eabae3754574c9e9ab669d8d404b80f11d9cbc
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-14 13:40:11 +0500

    78

--- main.java
+++ main.java
@@ -1,31 +1,24 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 class MyArrays{
-  private int arr1[] = new int[3];
-  private int arr2[] = new int[3];
-  private int result[] = new int[2];
-  public void printArrays(){
+  private int numbers[] = new int[2];
+  public void checkNumbers(){
     Scanner scanner = new Scanner(System.in);
-    for(int i = 0; i < 3; i++){
-      System.out.printf("\nEnter number# %d for array1: ",i);
-      arr1[i] = scanner.nextInt();
-      System.out.printf("Enter number# %d for array2: ",i);
-      arr2[i] = scanner.nextInt();
+    for(int i = 0; i < numbers.length; i++){
+      System.out.printf("\nEnter number# %d: ", i);
+      numbers[i] = scanner.nextInt();
+      
 }
-    
-    result[0] = arr1[0];
-    result[1] = arr2[arr2.length - 1];
-    System.out.printf("\nArray1: %s", Arrays.toString(arr1));
-    System.out.printf("\nArray2: %s", Arrays.toString(arr2));
-    
-    System.out.printf("\nResult: %s", Arrays.toString(result));
+
+    if(numbers[0] == 4 || numbers[0] == 7){System.out.printf("\nnumber# %d exists in array", numbers[0]);}
+    else if(numbers[1] == 4 || numbers[1] == 7){System.out.printf("\nnumber# %d exists in array", numbers[1]);}
+    else{System.out.print("none of 4 or 7 exists in array");}
 }
 }
 
 public class main{
   public static void main(String args[]){
-    MyArrays arr = new MyArrays();
-    arr.printArrays();
+    MyArrays arrays = new MyArrays();
+    arrays.checkNumbers();
 }
 }
