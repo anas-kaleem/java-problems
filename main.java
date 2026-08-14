6bfa225ab85dfef7cc342662eb25c588cfba45f3
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-14 13:25:16 +0500

    77

--- main.java
+++ main.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Arrays;
 
-class StringComparison{
-  private ArrayList<Integer> numbers1 = new ArrayList<>();
-  private ArrayList <Integer> numbers2 = new ArrayList<>();
-  public void compareStrings(){
+class MyArrays{
+  private int arr1[] = new int[3];
+  private int arr2[] = new int[3];
+  private int result[] = new int[2];
+  public void printArrays(){
     Scanner scanner = new Scanner(System.in);
+    for(int i = 0; i < 3; i++){
+      System.out.printf("\nEnter number# %d for array1: ",i);
+      arr1[i] = scanner.nextInt();
+      System.out.printf("Enter number# %d for array2: ",i);
+      arr2[i] = scanner.nextInt();
+}
     
-    for(int i = 0; i < 7; i++){
-      System.out.print("\nEnter the number for first array: ");
-      numbers1.add(scanner.nextInt());
-}//first-for
-    for(int i = 0; i < 11; i++){
-      System.out.print("\nEnter the number for second array: ");
-      numbers2.add(scanner.nextInt());
-}//second-for
-
-    if(numbers1.get(0) == numbers2.get(0)){System.out.print("\nfirst number of both strings is equal");}
-    else{System.out.print("\nfirst number of both strings is not equal");}
-    if(numbers1.get(numbers1.size()-1) == numbers2.get(numbers2.size() - 1)){System.out.print("\nlast number of both strings is equal");}
-    else{System.out.print("\nlast number of both strings is not equal");}
-}  
+    result[0] = arr1[0];
+    result[1] = arr2[arr2.length - 1];
+    System.out.printf("\nArray1: %s", Arrays.toString(arr1));
+    System.out.printf("\nArray2: %s", Arrays.toString(arr2));
+    
+    System.out.printf("\nResult: %s", Arrays.toString(result));
+}
 }
 
 public class main{
   public static void main(String args[]){
-    StringComparison comparison = new StringComparison();
-    comparison.compareStrings();
+    MyArrays arr = new MyArrays();
+    arr.printArrays();
 }
 }
