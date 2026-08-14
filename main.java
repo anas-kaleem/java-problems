0871e61c88ec9b3b72933b4b0f6b58859b3c4290
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-14 11:35:23 +0500

    73

--- main.java
+++ main.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
 
 class Characters{
-  private String text;
-  public void printText(){
+  private String text1;
+  private String text2;
+  private String result = "";
+  public void concatenateText(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter the text: ");
-    text = scanner.nextLine();
+    System.out.print("Enter first word or sentence: ");
+    text1 = scanner.nextLine();
+    System.out.print("Enter second word or sentence: ");
+    text2 = scanner.nextLine();
 
-   if(text.length() <= 3){
-     for(int i = 0; i < 3; i++){
-	System.out.print("#");
+    if(text1.length() == 0){
+      result = result + "#";
+      
+}//first-if
+
+    else{
+      result = result + text1.charAt(0);
+}//first-else-of-first-if
+  
+    if(text2.length() == 0){
+      result = result + "#";
+   
+}//second-if
+
+    else{
+      result = result + text2.charAt(text2.length() - 1);
 }
 
-    
-}
-
-   else{
-     for(int i = 0; i < 3; i++){
-	System.out.print(text.charAt(i));
-}
-}
-}
+    System.out.print(result);
+}		
 }
 
 public class main{
   public static void main(String args[]){
     Characters character = new Characters();
-    character.printText();
+    character.concatenateText();
 }
 }
