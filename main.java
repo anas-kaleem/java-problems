6d39c4246b1e54f3c5b16e47ddeca0723af7ac07
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-14 17:20:26 +0500

    81

--- main.java
+++ main.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class LargestValue{
+class Swapper{
   private int numbers[] = new int[3];
-  public int getLargestNumber(int number1, int number2){
-    if(number1 < number2){return number2;}
-    else{return number1;}
-}//getLargestNumber()
 
-  public void printLargestNumber(){
-    int largestNumber;
+  public int[] swapNumber(int[] numbers){
+     int temporary = numbers[0];
+     numbers[0] = numbers[numbers.length - 1];
+     numbers[numbers.length - 1] = temporary;
+     return numbers;
+     
+}//swapNumber
+  public void swapArray(){
     Scanner scanner = new Scanner(System.in);
     for(int i = 0; i < numbers.length; i++){
       System.out.printf("\nEnter number# %d: ", i);
       numbers[i] = scanner.nextInt();
-}//first-for-in-printLargestNumber()
-    
-    largestNumber = getLargestNumber(numbers[0], numbers[numbers.length - 1]);
-    System.out.print("\nOriginal Array: " + Arrays.toString(numbers));
-    System.out.printf("\nLargest value between first and last element: %d", largestNumber);
-}//printLargestNumber()
-
-}//class-LargestValue{}
+}//for
+  
+  System.out.print("\nOriginal Array: " + Arrays.toString(numbers));
+  System.out.print("\nNew Array after swapping: " + Arrays.toString(swapNumber(numbers)));  
+	    
+}
+}
 
 public class main{
   public static void main(String args[]){
-    LargestValue largestValue = new LargestValue();
-    largestValue.printLargestNumber();
+    Swapper swapper = new Swapper();
+    swapper.swapArray();
 }
 }
