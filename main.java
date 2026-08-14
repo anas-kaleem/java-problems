f818d65562a91a3da98eb1b85abdbdfe1b98a7c6
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-14 12:16:04 +0500

    76

--- main.java
+++ main.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-class Ten{
-  private int numbers[] = new int[7];
-  public void checkTen(){
+class StringComparison{
+  private ArrayList<Integer> numbers1 = new ArrayList<>();
+  private ArrayList <Integer> numbers2 = new ArrayList<>();
+  public void compareStrings(){
     Scanner scanner = new Scanner(System.in);
-    for(int i = 0; i < numbers.length; i++){
-      System.out.print("\nEnter the number: ");
-      numbers[i] = scanner.nextInt();
-}//first-for-loop
+    
+    for(int i = 0; i < 7; i++){
+      System.out.print("\nEnter the number for first array: ");
+      numbers1.add(scanner.nextInt());
+}//first-for
+    for(int i = 0; i < 11; i++){
+      System.out.print("\nEnter the number for second array: ");
+      numbers2.add(scanner.nextInt());
+}//second-for
 
-    if(numbers[0] == 10){System.out.printf("\nfirst number is %d", numbers[0]);}
-    else{System.out.print("\nfirst number is not 10");}
-    if(numbers[numbers.length - 1] == 10){System.out.printf("\nlast number is %d", numbers[numbers.length - 1]);}
-    else{System.out.print("\nlast number is not 10");}
-}//void-checkTen()
-}//class-Ten{}
+    if(numbers1.get(0) == numbers2.get(0)){System.out.print("\nfirst number of both strings is equal");}
+    else{System.out.print("\nfirst number of both strings is not equal");}
+    if(numbers1.get(numbers1.size()-1) == numbers2.get(numbers2.size() - 1)){System.out.print("\nlast number of both strings is equal");}
+    else{System.out.print("\nlast number of both strings is not equal");}
+}  
+}
 
 public class main{
   public static void main(String args[]){
-    Ten ten = new Ten();
-    ten.checkTen();
+    StringComparison comparison = new StringComparison();
+    comparison.compareStrings();
 }
 }
