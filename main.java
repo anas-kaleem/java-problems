a65710eae36cc34a1f2daabd67dbb721016a3499
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-15 11:14:47 +0500

    84

--- main.java
+++ main.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;
 
-class Product{
-
-  private int array1[] = new int[4];
-  private int array2[] = new int[4];
-  private int result[] = new int[4];
-  public void multiplyArrays(){
+class StringSum{
+  private String text;
+  private String subString = "";
+  public void changeText(){
     Scanner scanner = new Scanner(System.in);
-    for(int i = 0; i < array1.length; i++){
-      System.out.printf("\nEnter number# %d of array1: ",i);
-      array1[i] = scanner.nextInt();     
-}//first-for-loop
+    System.out.print("\nEnter the text: ");
+    text = scanner.nextLine();
 
-    for(int i = 0; i < array2.length; i++){
-      System.out.printf("\nEnter number# %d of array2: ",i);
-      array2[i] = scanner.nextInt();  
-}//second-for-loop
+    for(int i = text.length() - 3; i < text.length(); i++){
+      subString = subString + text.charAt(i);
+}//for-loop
 
-    for(int i = 0; i < array1.length; i++){
-      result[i] = (array1[i] * array2[i]);
-}
-    System.out.print("\nResult: ");
-    for(int i = 0; i < result.length; i++){
-      System.out.printf("%d ",result[i]);
-}
-}//multipleArrays()
-
-}//class-Multiplicaion
+    text = subString + text + subString;
+    System.out.print(text);
+}//chageText()
+}//class-StrignNum{}
 
 public class main{
   public static void main(String args[]){
-    Product product = new Product();
-    product.multiplyArrays();
+    StringSum stringSum = new StringSum();
+    stringSum.changeText();
 }
 }
