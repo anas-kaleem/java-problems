b77991f76442687bf6f6ad06d80c8fd75bde3d49
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-15 11:48:59 +0500

    85

--- main.java
+++ main.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 
-class StringSum{
+class Word{
   private String text;
-  private String subString = "";
-  public void changeText(){
+  private boolean isTrue = false;
+  public void isWord(){
     Scanner scanner = new Scanner(System.in);
     System.out.print("\nEnter the text: ");
     text = scanner.nextLine();
+    for(int i = 0; i < text.length(); i++){
+      int asciiValue = (int) (text.charAt(i));
+      if((asciiValue >= 97 && asciiValue <= 122) || (asciiValue >= 65 && asciiValue <= 90)){
+  	isTrue = true;
+   	break;
+}
 
-    for(int i = text.length() - 3; i < text.length(); i++){
-      subString = subString + text.charAt(i);
-}//for-loop
+}
 
-    text = subString + text + subString;
-    System.out.print(text);
-}//chageText()
-}//class-StrignNum{}
+    System.out.print(isTrue);
+}
+}
 
 public class main{
   public static void main(String args[]){
-    StringSum stringSum = new StringSum();
-    stringSum.changeText();
+    Word word = new Word();
+    word.isWord();
 }
 }
