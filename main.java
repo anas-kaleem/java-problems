878b03ab6d63eed46c37b0592aa2c2d576746bea
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-16 15:50:19 +0500

    95

--- main.java
+++ main.java
@@ -1,33 +1,26 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class Numbers{
-  private int numbers[] = new int[7];
-  public void sortNumbers(){
+class StringArray{
+  private String textArray[];
+  private int size;
+  public void populateArray(){
     Scanner scanner = new Scanner(System.in);
-    for(int i = 0; i < numbers.length; i++){
-      System.out.printf("\nEnter the number for index# %d: ",i);
-      numbers[i] = scanner.nextInt();
-}//first-for-in-sortNumbers();
-    int temp;
-    for(int i = 0; i < numbers.length; i++){
-      if((numbers[i] % 2 == 0) && (i < numbers.length - 1)){
-  	for(int j = i + 1; j < numbers.length; j++){
-  	  if(numbers[j] % 2 != 0){
- 	    temp = numbers[i]; numbers[i] = numbers[j]; numbers[j] = temp;
-	    break;
-}//if
-}//for
-}//if`
-}//second-for
+    System.out.print("Enter the size of the array: ");
+    size = scanner.nextInt();
+    
+    textArray = new String[size];
+    for(int i = 0; i < textArray.length; i++){
+      textArray[i] = Integer.toString(i);
+}
 
-    System.out.println("Sorted Array: " + Arrays.toString(numbers));
-}//void-sortNumbers();
-}//class-Numbers{}
+    System.out.print("\nString Array: " + Arrays.toString(textArray));
+}
+}
 
 public class main{
-  public static void main(){
-    Numbers number = new Numbers();
-    number.sortNumbers();
+  public static void main(String args[]){
+    StringArray stringArray = new StringArray();
+    stringArray.populateArray();
 }
 }
