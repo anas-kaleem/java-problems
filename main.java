215ff70abb041d86d7cd021c4bcd0af7b10b7800
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-16 15:11:31 +0500

    94

--- main.java
+++ main.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Numbers{
   private int numbers[] = new int[7];
-  private boolean isTrue = false;
-  public void checkNumbers(){
+  public void sortNumbers(){
     Scanner scanner = new Scanner(System.in);
     for(int i = 0; i < numbers.length; i++){
-      System.out.printf("\nEnter number for index# %d: ",i);
+      System.out.printf("\nEnter the number for index# %d: ",i);
       numbers[i] = scanner.nextInt();
-}//frist-for
-
-    for(int i = 1; i < numbers.length - 1; i++){
-      if(isTrue == true){
-	
-	break;
-}//first-if-in-second-for
-
-      else if(isTrue == false && (numbers[i] == 10 || numbers[i] == 20)){
-	if(numbers[i-1] == 10 && numbers[i + 1] == 10){isTrue = true;System.out.printf("\nNumber on left side of %d is: %d and on right it is: %d", numbers[i], numbers[i-1], numbers[i+1]);}
-	else if(numbers[i - 1] == 20 && numbers[i + 1] == 20){isTrue = true; System.out.printf("\nNumber on left side of %d is: %d and on right is : %d", numbers[i], numbers[i - 1], numbers[i + 1]);}
-}//first-elseif-of-first-if-in-second-for
+}//first-for-in-sortNumbers();
+    int temp;
+    for(int i = 0; i < numbers.length; i++){
+      if((numbers[i] % 2 == 0) && (i < numbers.length - 1)){
+  	for(int j = i + 1; j < numbers.length; j++){
+  	  if(numbers[j] % 2 != 0){
+ 	    temp = numbers[i]; numbers[i] = numbers[j]; numbers[j] = temp;
+	    break;
+}//if
+}//for
+}//if`
 }//second-for
-}
-}
+
+    System.out.println("Sorted Array: " + Arrays.toString(numbers));
+}//void-sortNumbers();
+}//class-Numbers{}
 
 public class main{
-  public static void main(String args[]){
+  public static void main(){
     Numbers number = new Numbers();
-    number.checkNumbers();
+    number.sortNumbers();
 }
 }
