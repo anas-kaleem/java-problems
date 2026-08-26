5ff65320abefbcf634965c405eb5c7de31503e5f
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-26 17:38:43 +0500

    java 101

--- main.java
+++ main.java
@@ -1,40 +1,31 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class ArrayPair{
-  private int array1[] = new int[5];
-  private int array2[] = new int[5];
-  
-  private int count = 0;
-  private int indexes[] = new int[5];
-  public void checkArrays(){
+
+class Numbers{
+  private int array[] = new int[7];
+  private int countTen = 0;
+  private int countTwenty = 0;
+  public void countNumbers(){
     Scanner scanner = new Scanner(System.in);
-    for(int i = 0; i < array1.length; i++){
-      System.out.printf("\nEnter %dth number of Array1: ",i+1);
-      array1[i] = scanner.nextInt();
-      System.out.printf("Enter %dthe number of Array2: ",i+1);
-      array2[i] = scanner.nextInt();
-}
-    int temp = 0;
-    for(int i = 0; i < array1.length; i++){
-      if(array1[i] - array2[i] <= 1){
-	count = count + 1;
-	indexes[temp] = 1;
-	temp = temp + 1;
-}
-      else{
-	indexes[temp] = 0;
-	temp = temp + 1;
-}
+    for(int i = 0; i < array.length; i++){
+      System.out.printf("\nEnter %dth number of array: ", i+1);
+      array[i] = scanner.nextInt();
+      
 }
 
-System.out.printf("\nTotal of: %d numbers have difference of 1 or less than 1, they are present at: ",count);
-System.out.print(Arrays.toString(indexes));
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == 10){countTen = countTen + 1;}
+      else if(array[i] == 20){countTwenty = countTwenty + 1;}
+}
+
+    if(countTen > countTwenty){System.out.printf("\nTen is more than twenty; Ten: %d Twenty: %d", countTen, countTwenty);}
+    else if(countTwenty > countTen){System.out.printf("\nTweny is more than Ten; Ten: %d Twenty: %d",countTwenty, countTen);}
+    else if(countTwenty == countTen){System.out.printf("\nTen and Twenty are equal Ten: %d Twenty: %d", countTen, countTwenty);}
 }
 }
 
 public class main{
   public static void main(String args[]){
-    ArrayPair arrayPair = new ArrayPair();
-    arrayPair.checkArrays();
+    Numbers number = new Numbers();
+    number.countNumbers();
 }
 }
