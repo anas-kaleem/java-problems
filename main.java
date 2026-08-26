0dfc812f6b983fd67ca6d86b7f265e3425a82f38
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-26 15:16:18 +0500

    98

--- main.java
+++ main.java
@@ -1,33 +1,45 @@
 import java.util.Scanner;
 
-class Twenty{
-  private int numbers[] = new int[8];
-  private boolean isUnique[] = {false, false, false};
-  
-  public void checkTwenty(){
+class Twenties{
+  private int numbers[] = new int[7];
+  private int count = 0;
+  private boolean isTrue = true;
+  private int indexes[] = new int[3];
+  public void checkTwenties(){
     Scanner scanner = new Scanner(System.in);
     for(int i = 0; i < numbers.length; i++){
-      System.out.printf("\nEnter the number for index# %d",i+1);
-      numbers[i] = scanner.nextInt();
-}
-    int index = 0;
-    for(int i = 0; i < numbers.length - 1; i++){
-      if((numbers[i] == 20) && (numbers[i+1] != 20)){
-	isUnique[index] = true;
-	index = index + 1;
-}
-}
+      System.out.printf("\nEnter %dth number: ", i+1);
+      numbers[i] = scanner.nextInt(); 
+}//1st-forLoop-in-checkTwenties()
 
-    if((isUnique[0] == true) && (isUnique[1] == true) && (isUnique[2] == true)){
-      System.out.print("\nYes! there are 3X twenties in your given array");
+int temp = 0;
+    for(int i = 0; i < numbers.length - 1; i++){
+      if(count == 3){
+ 	System.out.printf("\n3 consecutive twenties found at %d, %d, %d", indexes[0], indexes[1], indexes[2]);break;
+}//1st-if-in-2nd-forLoop-in-checkTwenties()
+      else if(isTrue == false){System.out.print("\nWarning! Twenties are next to each other.");break;
+}//1st-elseIf-in-2nd-forLoop-in-checkTwenties()
+      else if(numbers[i] == 20 && numbers[i+1] != 20){
+	count = count + 1;
+        indexes[temp] = i;
+	temp = temp + 1;
+}//2nd-elseIf-in-2nd-forLoop-in-checkTwenties()
+
+      else if(numbers[i] == 20 && numbers[i+1] == 20){isTrue = false;}
+}//2nd-forLoop-in-checkTwenties()
+
+
+  if(count < 3 && isTrue == true){
+    System.out.print("\nThere are less than 3 Twenties in the array.");
 }
-    else{System.out.print("\nNo! the twenties in your given array are either less than 3 or they are consecutive.");}
-}
-}
+    
+}//void-checkTwenties()
+}//class-Twenties{}
+
 
 public class main{
   public static void main(String args[]){
-    Twenty twenty = new Twenty();
-    twenty.checkTwenty();
+    Twenties twenties = new Twenties();
+    twenties.checkTwenties();
 }
 }
