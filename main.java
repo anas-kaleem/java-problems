2ee2609ebacc1e1e4b021e6fcd59f375ebd61ecd
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-27 14:30:29 +0500

    java 102

--- main.java
+++ main.java
@@ -2,30 +2,37 @@ import java.util.Scanner;
 
 class Numbers{
   private int array[] = new int[7];
-  private int countTen = 0;
-  private int countTwenty = 0;
-  public void countNumbers(){
+  private boolean isTen = false;
+  private boolean isThirty = false;
+  public void checkNumbers(){
     Scanner scanner = new Scanner(System.in);
     for(int i = 0; i < array.length; i++){
-      System.out.printf("\nEnter %dth number of array: ", i+1);
+      System.out.printf("\nEnter %dth element: ", i+1);
       array[i] = scanner.nextInt();
-      
 }
 
     for(int i = 0; i < array.length; i++){
-      if(array[i] == 10){countTen = countTen + 1;}
-      else if(array[i] == 20){countTwenty = countTwenty + 1;}
+      if(isTen == true && isThirty == true){
+	System.out.print("\nYes! both ten and thirty are present in array.");break;
+
 }
 
-    if(countTen > countTwenty){System.out.printf("\nTen is more than twenty; Ten: %d Twenty: %d", countTen, countTwenty);}
-    else if(countTwenty > countTen){System.out.printf("\nTweny is more than Ten; Ten: %d Twenty: %d",countTwenty, countTen);}
-    else if(countTwenty == countTen){System.out.printf("\nTen and Twenty are equal Ten: %d Twenty: %d", countTen, countTwenty);}
+    else if(array[i] == 10){isTen = true;}
+    else if(array[i] == 30){isThirty = true;}
+}
+
+    if(isTen == false){
+      System.out.print("\nTen is not found.");
+}
+    if(isThirty == false){
+      System.out.print("\nThirty is not found.");
+}
 }
 }
 
 public class main{
   public static void main(String args[]){
     Numbers number = new Numbers();
-    number.countNumbers();
+    number.checkNumbers();
 }
 }
