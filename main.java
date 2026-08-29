863798804119fed9190eb6e945fc69eebdd7ad59
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-29 21:16:27 +0500

    java 109

--- main.java
+++ main.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-class Addition{
-  private int inputNum;
-  private int result = 0;
-  private int temp;
-  public void addNumbers(){
+class Stairs{
+  private char coin = '$';
+  private char emptySpace = ' ';
+  public void createStairs(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter the number: ");
-    inputNum = scanner.nextInt();
-    while(inputNum > 0){
-      temp = inputNum % 10;
-      if((result + temp) < 10){
-	result = result + temp;
-	inputNum = inputNum / 10;
-}
-      else{break;}
-}
-
-    System.out.printf("Result is: %d", result);
+    int rows = 20;
+    int columns = 7;
+    for(int i = 1; i < rows; i++){
+      for(int j = 0; j <= columns; j++){
+	if((j == 0) || (j == columns)){System.out.print(coin);}
+ 	if(i % 2 == 0){
+  	  System.out.print(coin);
+}//if-in-2nd-for
+  	else{System.out.print(emptySpace);}
+ 	
+}//2nd-for
+	System.out.print("\n");
+}//first-for
 }
 }
 
 public class main{
   public static void main(String args[]){
-    Addition addition = new Addition();
-    addition.addNumbers();
+    Stairs stair = new Stairs();
+    stair.createStairs();
 }
 }
