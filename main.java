1a0207f57b5eff362c06da2df1a89b7fa36bc7ec
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-31 09:18:14 +0500

    java 109

--- main.java
+++ main.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
 
 class Stairs{
-  private char coin = '$';
-  private char emptySpace = ' ';
-  public void createStairs(){
+  private int inputNum;
+  public void printStairs(){
     Scanner scanner = new Scanner(System.in);
-    int rows = 20;
-    int columns = 7;
-    for(int i = 1; i < rows; i++){
-      for(int j = 0; j <= columns; j++){
-	if((j == 0) || (j == columns)){System.out.print(coin);}
- 	if(i % 2 == 0){
-  	  System.out.print(coin);
-}//if-in-2nd-for
-  	else{System.out.print(emptySpace);}
- 	
-}//2nd-for
+    System.out.print("\nEnter the number: ");
+    inputNum = scanner.nextInt();
+    int temp = inputNum;
+    for(int i = 1; i <= inputNum; i++){
+      for(int j = 1; j <= i; j++){
+	if(temp == 0){break;}
+	else{
+	  System.out.print("$");
+	  temp = temp - 1;
+}
+}
 	System.out.print("\n");
-}//first-for
+}
 }
 }
 
 public class main{
   public static void main(String args[]){
     Stairs stair = new Stairs();
-    stair.createStairs();
+    stair.printStairs();
 }
 }
