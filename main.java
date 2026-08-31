93f2ee46d4525693857fd639107a0d707032c20f
anas kaleem <muhammadanaskaleem@gmail.com>
2026-08-31 11:43:51 +0500

    java 111

--- main.java
+++ main.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 
-class Power{
-  private double inputNum;
-  public void checkNumber(){
+class Zeroes{
+  private long inputNum;
+  private long factorial = 1;
+  private long countZeroes = 0;
+  public void computeNumbers(){
     Scanner scanner = new Scanner(System.in);
     System.out.print("\nEnter the number: ");
-    inputNum = scanner.nextDouble();
-    while(inputNum > 1){
-      if((inputNum / 4) == 1){
-	inputNum = inputNum/4;
-  	System.out.print("Yes! the number is a power of 4.");
+    inputNum = scanner.nextInt();
+    for(int i = 1; i <= inputNum; i++){
+      factorial = (factorial * i);
+}
+    System.out.printf("\nFactorial: %d", factorial);
+    while(factorial > 0){
+      if((factorial % 10) != 0){
 	break;
 }
-      else{inputNum = inputNum / 4;}
+      else if((factorial % 10) == 0){countZeroes = countZeroes + 1; factorial = factorial / 10;}
 }
-
-    if(inputNum != 1){
-      System.out.print("\nNo! the number is not divided by 4.");
-}
+    System.out.printf("\nThe number of zeroes are: %d",countZeroes);
 }
 }
 
 public class main{
   public static void main(String args[]){
-    Power power = new Power();
-    power.checkNumber();
+    Zeroes zero = new Zeroes();
+    zero.computeNumbers();
 }
 }
