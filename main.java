ae8c5c298629621b8eb6495f25be24f5c19fcc63
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-05 12:17:49 +0500

    java 120

--- main.java
+++ main.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
-class ArrayOccurence{
+class Matrix{
+  private int numbers[][] = {{1,5,6,9,9,23},{23,8,10,14}};
   private int inputNum;
-  private int index;
   private boolean isTrue = false;
-  private int numbers[] = {2,4,8,9,3,10,20,2,5,4,55};
-  public void findNumber(){
+  public void searchMatrix(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter the number you want to search: ");
+    System.out.print("\nEnter the number that you want to search: ");
     inputNum = scanner.nextInt();
     for(int i = 0; i < numbers.length; i++){
-      if(numbers[i] == inputNum){
-      index = i;
-      isTrue = true;
-      break;
+      for(int j = 0; j < numbers[i].length; j++){
+	if(numbers[i][j] == inputNum){
+	  System.out.printf("\nYes! the number is present at: %dth column of %dth row", j + 1, i + 1);
+	  isTrue = true;
+	  break;
 }
 }
-
-  if(isTrue == true){
-    System.out.printf("\nCongrats! the number occurs at: %d", index);
+      if(isTrue == true){break;}
+}
+    if(isTrue == false){
+	System.out.print("\nSorry! number has not found.");
 }
 }
 }
 
 public class main{
   public static void main(String args[]){
-    ArrayOccurence arrayOccurence = new ArrayOccurence();
-    arrayOccurence.findNumber();
+    Matrix matrix = new Matrix();
+    matrix.searchMatrix();
 }
 }
