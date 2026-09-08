4f4a542aa698809edc1485486d488b38a0bf87f7
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-08 15:53:44 +0500

    java 121

--- main.java
+++ main.java
@@ -1,41 +1,57 @@
 import java.util.Scanner;
 
-class Index{
-  private int counter = 0;
-  private int index;
-  private int target;
-  
-  private boolean isTrue = false;
-  private int numbers[] = {2,3,5,6,8,11};
-  private int arrayTemp = (numbers[numbers.length - 1]) + 1;
-  private int indexTemp = numbers.length;
-  public void findIndex(){
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter the number: ");
-    target = scanner.nextInt();
-    while(isTrue == false){
-      if((counter < numbers.length && target < numbers[counter]) || (counter >= numbers.length && target < arrayTemp)){System.out.print("\nThe number is not in array!");break;}
-      if(counter < numbers.length && numbers[counter] == target){
-	isTrue = true;
-	index = counter;
-	
-}
-      else if(counter >= numbers.length && target == arrayTemp){
-	isTrue = true; index = indexTemp;
-	
-}
-      else if(counter >= numbers.length && target != arrayTemp){arrayTemp = arrayTemp + 1; indexTemp = indexTemp + 1;}
-      else{counter = counter + 1;}
+class Node{
+  int value;
+  Node next;
 }
 
-    if(isTrue == true){System.out.printf("\nThe number is at: %d",index);}
-    
+class Head{
+  Node head;
+}
+
+class Cursor{
+  Node current;
+}
+
+class List{
+  public void createList(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("\nEnter values for list: ");
+    Head heads = new Head();
+    Cursor cursor1 = new Cursor();
+    Cursor cursor2 = new Cursor();
+    for(int i = 1; i <= 10; i++){
+      if(i == 1){
+	Node node = new Node();
+        cursor1.current = node;
+	heads.head = node;
+        System.out.printf("\nEnter %dth value: ",i);
+	node.value = scanner.nextInt();
+}
+
+      else if(i > 1){
+	System.out.printf("\nEnter %dth value: ",i);
+  	cursor2.current = new Node();
+	cursor2.current.value = scanner.nextInt();
+	cursor1.current.next = cursor2.current;
+	cursor1.current = cursor1.current.next;
+}
+       
+}
+
+    cursor1.current = heads.head;
+    cursor2.current = heads.head;
+    for(int i = 1; i <= 10; i++){
+      System.out.printf("\n%dth value in linked list: %d",i, cursor1.current.value);
+      cursor1.current = cursor1.current.next;
+}
+    cursor1.current = heads.head;
 }
 }
 
 public class main{
   public static void main(String args[]){
-    Index indexObj = new Index();
-    indexObj.findIndex();
+    List lists = new List();
+    lists.createList();
 }
 }
