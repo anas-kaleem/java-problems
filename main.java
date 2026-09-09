fe9e8ec3d2b6ad341449cd4cb26fc9f34d91f83f
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-09 19:37:48 +0500

    java 122

--- main.java
+++ main.java
@@ -1,64 +1,30 @@
 import java.util.Scanner;
 
-class Node{
-  int value;
-  Node next;
-  Node prev;
+class SubArrays{
+  private int numbers[] = {2,5,6,-2,-3,1,6,9};
+  private int start = 0;
+  private int end = 0;
+  private String occurence = "";
+  private int result = numbers[0];
+  private int sum = 0;
+  public void computeSum(){
+    for(int i = 0; i < numbers.length; i++){
+      sum = sum + numbers[i];
+      if(sum > result){
+	result = sum;
+	occurence = (start + 1)+ " to " + (end + 1);
+	start = end + 1;
+	sum = 0;
 }
-
-class Head{
-  Node head;
+      end = end + 1;
 }
-
-class Current{
-  Node current;
+    System.out.printf("\nThe biggest sum is from %s", occurence);
 }
-
-class List{
-  private int size = 0;
-  public void createList(){
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter first number: ");
-    Head header = new Head();
-    Current back = new Current();
-    Current front = new Current();
-    header.head = new Node();
-    front.current = header.head;
-    back.current = header.head;
-    header.head.value = scanner.nextInt();
-
-    for(int i = 2; i <= 7; i++){
-      System.out.printf("\nEnter %dth number: ", i);
-      front.current = new Node();
-      front.current.value = scanner.nextInt();
-      back.current.next = front.current;
-      front.current.prev = back.current;
-      back.current = front.current;
-      size = size + 1;
-}//first-for-createList()
-    front.current = header.head;
-    int temp;
-    for(int j = 1; j <= (size / 2); j++){
-      temp = front.current.value;
-      front.current.value = back.current.value;
-      back.current.value = temp;
-      front.current = front.current.next;
-      back.current = back.current.prev;
 }
-
-    front.current = header.head;
-    for(int i = 1; i <= 7; i++){
-      System.out.printf("\n%d", front.current.value);
-      front.current = front.current.next;
-}
-}//void-createList()
-  
- 
-}//class-List{}
 
 public class main{
   public static void main(String args[]){
-    List lists = new List();
-    lists.createList();
+    SubArrays subArray = new SubArrays();
+    subArray.computeSum();
 }
 }
