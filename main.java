f1423efe22c1a4bc9c4b944baedcdcba4d896c2a
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-09 09:40:55 +0500

    java 121

--- main.java
+++ main.java
@@ -3,51 +3,58 @@ import java.util.Scanner;
 class Node{
   int value;
   Node next;
+  Node prev;
 }
 
 class Head{
   Node head;
 }
 
-class Cursor{
+class Current{
   Node current;
 }
 
 class List{
+  private int size = 0;
   public void createList(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter values for list: ");
-    Head heads = new Head();
-    Cursor cursor1 = new Cursor();
-    Cursor cursor2 = new Cursor();
-    for(int i = 1; i <= 10; i++){
-      if(i == 1){
-	Node node = new Node();
-        cursor1.current = node;
-	heads.head = node;
-        System.out.printf("\nEnter %dth value: ",i);
-	node.value = scanner.nextInt();
+    System.out.print("\nEnter first number: ");
+    Head header = new Head();
+    Current back = new Current();
+    Current front = new Current();
+    header.head = new Node();
+    front.current = header.head;
+    back.current = header.head;
+    header.head.value = scanner.nextInt();
+
+    for(int i = 2; i <= 7; i++){
+      System.out.printf("\nEnter %dth number: ", i);
+      front.current = new Node();
+      front.current.value = scanner.nextInt();
+      back.current.next = front.current;
+      front.current.prev = back.current;
+      back.current = front.current;
+      size = size + 1;
+}//first-for-createList()
+    front.current = header.head;
+    int temp;
+    for(int j = 1; j <= (size / 2); j++){
+      temp = front.current.value;
+      front.current.value = back.current.value;
+      back.current.value = temp;
+      front.current = front.current.next;
+      back.current = back.current.prev;
 }
 
-      else if(i > 1){
-	System.out.printf("\nEnter %dth value: ",i);
-  	cursor2.current = new Node();
-	cursor2.current.value = scanner.nextInt();
-	cursor1.current.next = cursor2.current;
-	cursor1.current = cursor1.current.next;
+    front.current = header.head;
+    for(int i = 1; i <= 7; i++){
+      System.out.printf("\n%d", front.current.value);
+      front.current = front.current.next;
 }
-       
-}
-
-    cursor1.current = heads.head;
-    cursor2.current = heads.head;
-    for(int i = 1; i <= 10; i++){
-      System.out.printf("\n%dth value in linked list: %d",i, cursor1.current.value);
-      cursor1.current = cursor1.current.next;
-}
-    cursor1.current = heads.head;
-}
-}
+}//void-createList()
+  
+ 
+}//class-List{}
 
 public class main{
   public static void main(String args[]){
