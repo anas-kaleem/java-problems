9fcfc5684af58eb23041b392a8fef59792d6377a
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-10 07:19:14 +0500

    java 123

--- main.java
+++ main.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 class SubArrays{
-  private int numbers[] = {2,5,6,-2,-3,1,6,9};
+  private int numbers[] = {-2,-3,2,5,1,-15,-1,4};
   private int start = 0;
   private int end = 0;
-  private String occurence = "";
-  private int result = numbers[0];
   private int sum = 0;
+  private int result = numbers[0];
+  private String occurence = "";
   public void computeSum(){
     for(int i = 0; i < numbers.length; i++){
       sum = sum + numbers[i];
-      if(sum > result){
+      if(sum < result){
 	result = sum;
-	occurence = (start + 1)+ " to " + (end + 1);
-	start = end + 1;
-	sum = 0;
+  	occurence = (start + 1) + " to " + (end + 1);
+  	start = end + 1;
+  	sum = 0;
 }
       end = end + 1;
+      
 }
-    System.out.printf("\nThe biggest sum is from %s", occurence);
+
+    System.out.printf("\nSubarray with smalles sum starts from %s", occurence);
 }
 }
 
