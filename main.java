49d943588cc61b641fbc3c3cb132aa43188e4735
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-13 18:43:23 +0500

    java 144

--- main.java
+++ main.java
@@ -1,38 +1,50 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class NthElement{
+class Elements{
   private int numbers[];
-  private int index;
+  private int resultArray[];
   private int size;
-  private int resultList[];
-  public void removeElement(){
+  private int index;
+  private int target;
+  private int tempArray[];
+  public void removeElements(){
     Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter the size of the list: ");
+    System.out.print("\nEnter size of array: ");
     size = scanner.nextInt();
     numbers = new int[size];
-    for(int i = 0; i < numbers.length; i++){
-      System.out.printf("\nEnter %dth number: ",i + 1);
+    for(int i = 0; i < size; i++){
+      System.out.printf("\nEnter %dth number: ", i + 1);
       numbers[i] = scanner.nextInt();
 }
-    System.out.print("\nEnter the nth element to remove: ");
-    index = (scanner.nextInt() - 1);
-    resultList = new int[size - 1];
-    int temp = 0;
+    tempArray = new int[size];
+    tempArray = Arrays.copyOf(numbers,size);
+    System.out.print("\nEnter the number you want to remove: ");
+    target = scanner.nextInt();
     for(int i = 0; i < numbers.length; i++){
-      if(i != index){
-        resultList[temp] = numbers[i];
-	temp = temp + 1;
+      if(numbers[i] == target){
+	size = size - 1;
+	resultArray = new int[size];
+	int temp = 0;
+  	for(int j = 0; j < numbers.length; j++){
+	  if(j != i){
+	    resultArray[temp] = numbers[j];
+	    temp = temp + 1;
 }
 }
-    System.out.print("\nOriginal Array: " + Arrays.toString(numbers));
-    System.out.print("\nResultant Array: " + Arrays.toString(resultList));
+  	numbers = new int[size];
+	numbers = Arrays.copyOf(resultArray,size);
+}
+}
+
+    System.out.print("\nOriginal Array: " + Arrays.toString(tempArray));
+    System.out.print("\nResultant Array: " + Arrays.toString(resultArray));
 }
 }
 
 public class main{
   public static void main(String args[]){
-    NthElement nthElement = new NthElement();
-    nthElement.removeElement();
+    Elements element = new Elements();
+    element.removeElements();
 }
 }
