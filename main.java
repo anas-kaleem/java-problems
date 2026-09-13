481d91940c56a8030ab97c86e16bd949504d62b8
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-13 19:59:33 +0500

    java 143

--- main.java
+++ main.java
@@ -1,50 +1,84 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
-class Elements{
-  private int numbers[];
-  private int resultArray[];
-  private int size;
-  private int index;
-  private int target;
-  private int tempArray[];
-  public void removeElements(){
+class Pointer{
+  Node pointer;
+}
+
+class Node{
+  int value;
+  Node next;
+}
+
+class MergedLists{
+  private int size1;
+  private int size2;
+  public void mergeLists(){
+    Pointer firstHead = new Pointer();
+    Pointer firstCurrent1 = new Pointer();
+    Pointer firstCurrent2 = new Pointer();
+    Pointer firstTail = new Pointer();
+    firstHead.pointer = new Node();
+    firstCurrent1.pointer = firstHead.pointer;
+    firstCurrent2.pointer = firstHead.pointer;
+
     Scanner scanner = new Scanner(System.in);
-    System.out.print("\nEnter size of array: ");
-    size = scanner.nextInt();
-    numbers = new int[size];
-    for(int i = 0; i < size; i++){
-      System.out.printf("\nEnter %dth number: ", i + 1);
-      numbers[i] = scanner.nextInt();
+    System.out.print("\nEnter size of list1: ");
+    size1 = scanner.nextInt();
+    System.out.print("\nEnter 1st number of list1: ");
+    firstCurrent2.pointer.value = scanner.nextInt();
+    for(int i = 2; i <= size1; i++){
+      System.out.printf("Enter %dth value of list1: ", i);
+      firstCurrent2.pointer = new Node();
+      firstCurrent2.pointer.value = scanner.nextInt();
+      firstCurrent1.pointer.next = firstCurrent2.pointer;
+      firstCurrent1.pointer = firstCurrent2.pointer;
 }
-    tempArray = new int[size];
-    tempArray = Arrays.copyOf(numbers,size);
-    System.out.print("\nEnter the number you want to remove: ");
-    target = scanner.nextInt();
-    for(int i = 0; i < numbers.length; i++){
-      if(numbers[i] == target){
-	size = size - 1;
-	resultArray = new int[size];
-	int temp = 0;
-  	for(int j = 0; j < numbers.length; j++){
-	  if(j != i){
-	    resultArray[temp] = numbers[j];
-	    temp = temp + 1;
-}
-}
-  	numbers = new int[size];
-	numbers = Arrays.copyOf(resultArray,size);
-}
+    firstTail.pointer = firstCurrent2.pointer;
+    firstCurrent1.pointer = firstHead.pointer;
+    firstCurrent2.pointer = firstHead.pointer;
+
+    System.out.print("\n");
+
+    Pointer secndHead = new Pointer();
+    Pointer secndCurrent1 = new Pointer();
+    Pointer secndCurrent2 = new Pointer();
+    Pointer secndTail = new Pointer();
+
+    secndHead.pointer = new Node();
+    secndCurrent1.pointer = secndHead.pointer;
+    secndCurrent2.pointer = secndHead.pointer;
+    System.out.print("\nEnter the size of list2: ");
+    size2 = scanner.nextInt();
+    System.out.print("\nEnter 1st value of list2: ");
+    secndHead.pointer.value = scanner.nextInt();
+
+    for(int j = 2; j <= size2; j++){
+      secndCurrent2.pointer = new Node();
+      System.out.printf("Enter %dth value of list2: ", j);
+      secndCurrent2.pointer.value = scanner.nextInt();
+      secndCurrent1.pointer.next = secndCurrent2.pointer;
+      secndCurrent1.pointer = secndCurrent2.pointer;
 }
 
-    System.out.print("\nOriginal Array: " + Arrays.toString(tempArray));
-    System.out.print("\nResultant Array: " + Arrays.toString(resultArray));
+    secndTail.pointer = secndCurrent2.pointer;
+    secndCurrent1.pointer = secndHead.pointer;
+    secndCurrent2.pointer = secndHead.pointer;
+
+    firstTail.pointer.next = secndHead.pointer;
+
+    for(int l = 1; l <= (size1 + size2); l++){
+      System.out.printf("\nvalue at %d is: %d", l, firstCurrent1.pointer.value);
+      firstCurrent1.pointer = firstCurrent1.pointer.next;
+}
+
+      firstCurrent1.pointer = firstHead.pointer;
 }
 }
 
 public class main{
   public static void main(String args[]){
-    Elements element = new Elements();
-    element.removeElements();
+    MergedLists mergedList = new MergedLists();
+    mergedList.mergeLists();
 }
 }
+
