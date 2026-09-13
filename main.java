878f12e293cbafa3a065185b7665f2394e6dcc70
anas kaleem <muhammadanaskaleem@gmail.com>
2026-09-13 16:36:25 +0500

    java 148

--- main.java
+++ main.java
@@ -1,77 +1,43 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
-class Duplicates{
-  private int numbers[] = {1,1,2,3,3,4,5,6,7,7,7,7};
-  private int arrayCopy[];
-  private int tempArray[];
-  private int count;
-  private int sizeAscend = 0;
-  private int sizeDescend = numbers.length;
-  private int after;
+class Characters{
+  private String userText;
+  private int count = 0;
   private boolean isTrue = false;
-  private int temp;
-
-  public void removeDup(){
-    for(int i = 0; i < numbers.length; i++){
-      	after = 1;
-	count = 1;
-	isTrue = false;
-       	temp = numbers[0];
-       	numbers[0] = numbers[i];
-      	numbers[i] = temp;
-      	for(int j = 1; j < numbers.length; j++){
-	  if(numbers[j] == numbers[0]){
- 	    count = count + 1;
-}
-	if(count == 2  && i == 0){
+  private int occurence;
+  private int after;
+  public void findUnique(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("\nEnter the text: ");
+    userText = scanner.nextLine();
+    for(int i = 0; i < userText.length() - 1; i++){
+      isTrue = false;
+      count = 0;
+      after = i + 1;
+      for(int j = after; j < userText.length(); j++){
+   	if(userText.charAt(i) == userText.charAt(j)){
 	  isTrue = true;
-	  sizeDescend = sizeDescend - 1;
-	  arrayCopy = new int[sizeDescend];
-  	  temp = 0;  
-	  int temp2;
-	  for(int k = 0; k < numbers.length; k++){
-	    if(number.length != numbers[0]){
-	      arrayCopy[temp] = numbers[k];
-	      temp = temp + 1;
+	  count = count + 1;
 }
 }
-}
-	else if(count == 2 && i > 0){
-	  isTrue = true;
-	  temp = numbers[0];
-	  sizeDescend = sizeDescend - 1; 
-}
-	if(isTrue == true){
-	  boolean makeArray = true;
-	  for(int l = 0; l < arrayCopy.length; l++){
-	    if(arrayCopy[l] == numbers[0]){
-	      makeArray = false;
-	      break;
+	if(isTrue == false && count == 0){  
+	  occurence = i + 1;
+	  break;
 }
 }
-	  if(makeArray == true){
-	    arrayCopy = new int[sizeDescend];
-  	    temp = 0;  
-	    for(int k = 0; k < numbers.length; k++){
-	      if(number.length != numbers[0]){
-	        arrayCopy[temp] = numbers[k];
-	        temp = temp + 1;
-}
-}
-}
+    if(isTrue == false){
+      System.out.printf("\nYes! there is a unique letter at: %d", occurence);
 }
 
- 	else if(isTrue == false){arrayCopy = Arrays.copyOf(numbers);}
+    else if(isTrue == true){
+      System.out.printf("\nNo! there is no unique letter in String");
 }
-
-    System.out.print("Resultant array: " + Arrays.toString(arrayCopy));
 }
 }
 
 public class main{
   public static void main(String args[]){
-    Duplicate duplicate = new Duplicate();
-    duplicate.removeDup();
+    Characters character = new Characters();
+    character.findUnique();
 }
 }
